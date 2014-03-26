8858d806212907a0f6e46ea482a61e8ce029d5f8
neal jones <alwaysworkharder@gmail.com>
2014-03-26 10:52:48 -0700

    displaying image and name, needs styling

--- a_cat/views.py
+++ a_cat/views.py
@@ -27,7 +27,8 @@ def get_fb_post(request):
 
     found_post = None
     while not found_post:
-        friend = random.choice(friends)  # Gets random friend
+        friend = random.choice(friends)  # Gets random
+        print friend
         friend_id = friend['id']  # sets random friend equal to variable
         resp = requests.get("https://graph.facebook.com/{0}/posts/?access_token={1}&limit=100".format(friend_id, settings.FACEBOOK_APP_TOKEN))
         posts = resp.json()
@@ -36,6 +37,6 @@ def get_fb_post(request):
                 if 'status_type' in post and post['status_type'] == 'wall_post':
                     found_post = post
 
-    data = {'friend_posts': found_post, 'picture': 'https://graph.facebook.com/{0}/picture?type=large'.format(friend_id), 'name': 'https://graph.facebook.com/{0}/name?'.format(friend_id)}
+    data = {'friend_posts': found_post, 'picture': 'https://graph.facebook.com/{0}/picture?type=large'.format(friend_id), 'name': friend['name']}
 
     return HttpResponse(json.dumps(data), mimetype='application/json')
